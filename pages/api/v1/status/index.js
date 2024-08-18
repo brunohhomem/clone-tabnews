ca137b9e7c2ce8cd93d5a9db798472f27a535f62
Bruno H. Homem <43353129+brunohhomem@users.noreply.github.com>
2024-08-18 19:18:43 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-  response.status(200).json({chave: "ausdhuahdasudhasudashd"});
+import database from "../../../../infra/databse";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  response.status(200).json({ chave: "ausdhuahdasudhasudashd" });
 }
 
 export default status;
